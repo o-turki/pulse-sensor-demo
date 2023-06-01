e01774f4dc1c9a073fba22ba1a998b45c068f7bf
Othman Turki <o.turki@advantryx.com>
2023-06-01 16:43:32 +0100

    nc

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char *ssid = "O-TURKI";      // "REPLACE_WITH_YOUR_SSID"
+const char *password = "01234567"; // "REPLACE_WITH_YOUR_PASSWORD"
+
+String endPoint = "http://192.168.137.1/iomt_lab/";
+String reqJSON = "";
+String resJSON = "";
 
 //  Variables
 unsigned long previousMillisGetHR = 0;
@@ -8,29 +17,39 @@ const long intervalGetHR = 20;
 const long intervalHR = 10000;
 
 int PulseSensorHRWire = 32; // Pulse Sensor WIRE connected to ANALOG PIN 32
-int LED = LED_BUILTIN;      //  The on-board ESP32 LED (=2)
+int LED = LED_BUILTIN;      //  The on-board ESP32 LED (2)
 
-// int Signal;           // holds the incoming raw data. Signal value can range from 0-4095
-int Threshold = 2970; // Determine which Signal to "count as a beat", and which to ingore.
+int Threshold = 3000; // Determine which Signal to "count as a beat", and which to ingore.
 
 int cntHB = 0;
 boolean ThresholdStat = true;
 int BPMval = 0;
 
 void GetHeartRate();
+String httpPOSTRequest(String endPoint, String postData);
 
 void setup()
 {
   Serial.begin(115200);
-  delay(500);
-
   pinMode(LED, OUTPUT); // pin that will blink to your heartbeat!
 
-  Serial.println();
-  Serial.println("Please wait 10 seconds to get the BPM Value");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.println("\n================================================================================");
+  Serial.println("> Connected to WiFi network \"" + WiFi.SSID() + "\"");
+  Serial.println("> IP Address: " + WiFi.localIP().toString());
+  Serial.println("> MAC Address: " + WiFi.macAddress());
+  Serial.println("================================================================================");
 }
 
-// The Main Loop Function
 void loop()
 {
   GetHeartRate();
@@ -51,6 +70,7 @@ void GetHeartRate()
     previousMillisGetHR = currentMillisGetHR;
 
     int PulseSensorHRVal = analogRead(PulseSensorHRWire);
+    Serial.println(PulseSensorHRVal);
 
     if (PulseSensorHRVal > Threshold && ThresholdStat == true)
     {
@@ -74,9 +94,32 @@ void GetHeartRate()
     previousMillisHR = currentMillisHR;
 
     BPMval = cntHB * 6;
-    Serial.print("BPM : ");
-    Serial.println(BPMval);
+    // Serial.print("BPM : ");
+    // Serial.println(BPMval);
 
     cntHB = 0;
+
+    // SEND DATA TO DB
+    reqJSON = "{\"BPM\":" + String(BPMval) + "}";
+    resJSON = httpPOSTRequest(endPoint, reqJSON);
   }
+}
+
+String httpPOSTRequest(String endPoint, String postData)
+{
+  HTTPClient http;
+  http.begin(endPoint);
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponseCode = http.POST(postData);
+
+  String payload = "{\"error\": \"CONNECTION_ERROR\"}";
+  if (httpResponseCode > 0)
+  {
+    payload = http.getString();
+  }
+
+  http.end();
+
+  return payload;
 }
